5ba043a563f08e6f980fb3e870f74868b73b4003
amarulabs <galarzadavid14@gmail.com>
2016-05-18 18:17:20 -0500

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 include('_start.php');
 $databasemanagar->addConnection([
     'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'xxx',
-    'username'  => 'ttt',
-    'password'  => 'zzz',
+    'host'      => 'ec2-54-235-132-192.compute-1.amazonaws.com',
+    'database'  => 'dfqaeq78ooec20',
+    'username'  => 'rbvpfxvyrnljdv',
+    'password'  => 'q2v2KDHn4CKLYm1WNRM9LU_etO',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
